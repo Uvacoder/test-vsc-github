76b4c475a6cf1923203f865e39378bbf15c097b6
vishwas <batty.wayn3@gmail.com>
2017-09-12 22:25:15 +0530

    add header component

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vishwas3 on 12/9/17.
+ */
+
+import React from 'react'
+import Title from './index/title/Title';
+import Navigation from './index/navigation/Navigation';
+
+export const Header = () => (
+	<div>
+		<Title />
+		<Navigation />
+	</div>
+)
+
+export default Header;
