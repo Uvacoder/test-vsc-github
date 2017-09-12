ac581ee29d1a847874e62e6db1315d1f8046c43d
vishwas <batty.wayn3@gmail.com>
2017-09-12 21:17:46 +0530

    addded title component styles

--- components/index/title/Title.js
+++ components/index/title/Title.js
@@ -5,8 +5,21 @@
 import React from 'react'
 
 export const Title = () => (
-	<div>
-		Jeremy Washington
+	<div className="head">
+		Vishwas Singh Chouhan
+		<style jsx>{`
+			.head{
+				text-align: center;
+				font-size: 2.5rem;
+				letter-spacing: .35rem;
+				font-weight: bold;
+				cursor: pointer;
+			}
+			.head :hover{
+				color: #a8a8a8;
+			}
+		`}
+		</style>
 	</div>
 )
 
