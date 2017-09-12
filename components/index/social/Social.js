582594d5108c93e0aa3fab2687c9e397df908bc7
vishwas <batty.wayn3@gmail.com>
2017-09-12 23:47:25 +0530

    add links to social

--- components/index/social/Social.js
+++ components/index/social/Social.js
@@ -6,10 +6,19 @@ import React from 'react'
 
 export const Social = () => (
 	<div className="social">
-		<img src="static/img/linkedin.png" alt="Linkedin"/>
-		<img src="static/img/github.png" alt="Github"/>
-		<img src="static/img/stackoverflow.png" alt="Stackoverflow"/>
-		<img src="static/img/twitter.png" alt="Twitter"/>
+		<a href="https://www.linkedin.com/in/vschouhan/" target="_blank">
+			<img src="static/img/linkedin.png" alt="Linkedin"/>
+		</a>
+		<a href="https://github.com/vsc-github" target="_blank">
+			<img src="static/img/github.png" alt="Github"/>
+		</a>
+		<a href="https://stackoverflow.com/users/5005669/vishwas-singh-chouhan" target="_blank">
+			<img src="static/img/stackoverflow.png" alt="Stackoverflow"/>
+		</a>
+		<a href="https://twitter.com/emergenturd" target="_blank">
+			<img src="static/img/twitter.png" alt="Twitter"/>
+		</a>
+
 		<style jsx>{`
 			.social{
 				text-align: center;
