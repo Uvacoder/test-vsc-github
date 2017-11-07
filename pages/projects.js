b31e8ff30a6dc94f8372e826977ccaf9984b529c
vishwas <batty.wayn3@gmail.com>
2017-11-08 00:50:21 +0530

    use ProjectsHero component in project page

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,9 @@
 import 'isomorphic-fetch';
 import React from 'react';
 import Head from 'next/head'
+import ReactCursorPosition from 'react-cursor-position';
 import TypedBio from '../components/TypedBio';
+import ProjectsHero from '../components/ProjectsHero';
 
 class Projects extends React.Component {
 
@@ -22,6 +24,10 @@ class Projects extends React.Component {
 					<title>Side Projects - Vishwas Singh Chouhan</title>
 					<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
 				</Head>
+
+				<ReactCursorPosition>
+					<ProjectsHero/>
+				</ReactCursorPosition>
 
 				<TypedBio
 					strings={[
