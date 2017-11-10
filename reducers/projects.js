ee44088d8299359bf399ecf550c985f1d8757aa1
vishwas <batty.wayn3@gmail.com>
2017-11-10 18:20:25 +0530

    add classwroom project to the store with image

--- reducers/projects.js
+++ reducers/projects.js
@@ -13,68 +13,12 @@ export default function(state = {
 			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
 		},
 		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
-		{
-			title: 'React-redux-offline-starter',
-			description: 'This is a simple boilerplate to start a react-redux project right away with offline caching support by deafult.',
-			imageUrl: 'https://s1.postimg.org/1jiuosltzz/damian-watracz-design-project-unstoppable-foundation.jpg',
-			codeUrl: 'https://github.com/vsc-github/simplest-offline-react-redux-starter',
-			demoUrl: 'https://vsc-github.github.io/simplest-offline-react-redux-starter/'
-		},
+			title: 'Classwroom',
+			description: 'I started Classwroom.com during the final year of my college to solve the hassle of offline aptitude tests in college.',
+			imageUrl: 'static/img/projects/classwroom.jpg',
+			codeUrl: '',
+			demoUrl: 'http://classwroom.com/'
+		}
 	]
 }, action) {
 	const { type } = action;
