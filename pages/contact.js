e6de4d87fb6c245d48317dfdf66887b3112a83cc
vishwas <batty.wayn3@gmail.com>
2017-11-30 19:13:53 +0530

    remove back button from contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,19 +1,37 @@
 import 'isomorphic-fetch';
 import React from 'react';
-import BackButton from '../components/BackButton';
 import WebFont from '../components/WebFont';
 import Superman from '../components/Superman/Superman';
+import Header from '../components/Header';
+
 class Experience extends React.Component {
 
 	render() {
 		return (
-			<div className="soon">
-				<BackButton/>
+			<div className="contacts-container">
 				<WebFont/>
-
+				<Header/>
 				<Superman/>
 
 				<style jsx>{`
+
+				.contacts-container{
+						padding: 0;
+						max-width: 800px;
+						margin: 0 auto;
+						padding: 1rem 1rem 0 1rem;
+				}
+				@media screen and (min-width: 600px) {
+					.contacts-container {
+							padding: 5rem 5rem 0 5rem;
+					}
+				}
+
+				@media screen and (min-width: 1000px) {
+						.contacts-container {
+							padding: 5rem 5rem 0 5rem;
+					}
+				}
 				`}</style>
 			</div>
 		);
